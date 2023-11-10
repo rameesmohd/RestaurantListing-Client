a639efb9b6d8ba34661ca3ea1e6405f4a2e701a5
Rameez <rameesmohd789@gmail.com>
2023-11-10 22:15:20 +0530

    removed unnecessary code in formModal

--- Client/src/Components/FormModal.jsx
+++ Client/src/Components/FormModal.jsx
@@ -84,7 +84,7 @@ const Modal=({loading,role,obj,title,setShowModal,action})=>{
               <form>
               <div className="px-3 flex-auto">
                 <div onMouseOver={()=>setMouseOver(true)} onMouseOut={() => setMouseOver(false)} className="flex items-center justify-center w-full pb-1">
-                { role==='edit' && (obj?.image ? (
+                { role==='edit' && obj?.image && (
                    <div onClick={handleFileClick} className="relative cursor-pointer flex justify-center items-center">
                    <div className={`absolute ${mouseOver ? '' : 'hidden'}`}>
                      <EditIcon />
@@ -101,17 +101,7 @@ const Modal=({loading,role,obj,title,setShowModal,action})=>{
                     onChange={(e)=>setImage(e.target.files[0])}
                     ref={fileInputRef} />
                  </div>
-                ) :  
-                <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-52 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 ">
-                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                          <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                          </svg>
-                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                          <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                      </div>
-                      <input id="dropzone-file" onChange={(e)=>setImage(e.target.files[0])} type="file" className="hidden" />
-                </label> )}
+                )}
 
                 { role==='add' && (image ? <img src={image instanceof File ? URL.createObjectURL(image):''} className="h-64 object-cover w-72" alt="" /> :
                   <label for="dropzone-file" className="flex flex-col items-center justify-center w-full h-52 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800  hover:bg-gray-100 ">
