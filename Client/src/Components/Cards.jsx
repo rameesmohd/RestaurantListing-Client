167e5722c9f02d1e0422283f7f2bd72ce3585e93
Rameez <rameesmohd789@gmail.com>
2023-11-10 22:50:01 +0530

    fixed alignment issue in card

--- Client/src/Components/Cards.jsx
+++ Client/src/Components/Cards.jsx
@@ -13,26 +13,27 @@ const card = ({loading,obj,deleteData,updateData}) => {
       <img className="w-96 max-w-96 h-44" src={obj.image}  alt="Sunset in the mountains"/>
         <div className="px-6 py-4 min-h-24">
           <div className="font-bold text-xl mb-2">{obj.name}</div>
-            <p className="text-gray-700 text-base break-words whitespace-normal min-h-[1.5rem] line-clamp-2">
+            <p className="text-gray-700 text-base break-words whitespace-normal min-h-[3rem] line-clamp-2">
               {obj.address}
             </p>
           </div>
           <div className="px-6 pt-4 pb-3  flex justify-between">
-          <div>
-            <ContactPhoneIcon className='mx-2'/>
-            {obj.contact}
-          </div>
-          <div>
-            <IconButton onClick={()=>setShowModal(true)}>
-              <EditNoteIcon  className='mx-2 hover:scale-105 cursor-pointer'/>
-            </IconButton>
-            <IconButton onClick={()=>deleteData(obj.id)}>
-              <DeleteOutlineIcon className='mx-2 hover:scale-105 cursor-pointer'/>
-            </IconButton>
-          </div>
+            <div>
+              <ContactPhoneIcon className='mx-2'/>
+              {obj.contact}
+            </div>
+            <div>
+              <IconButton onClick={()=>setShowModal(true)}>
+                <EditNoteIcon className='mx-2 hover:scale-105 cursor-pointer'/>
+              </IconButton>
+              <IconButton onClick={()=>deleteData(obj.id)}>
+                <DeleteOutlineIcon className='mx-2 hover:scale-105 cursor-pointer'/>
+              </IconButton>
+            </div>
         </div>
     </div>
-    { showModal && <FormModal 
+    { showModal && 
+    <FormModal 
     role={'edit'} 
     title={'Edit restaurant'} 
     obj={obj}
